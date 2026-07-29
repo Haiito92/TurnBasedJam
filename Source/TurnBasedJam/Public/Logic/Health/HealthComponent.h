f2cd93d7c905ea37d81101fdbe93f8f7b4309cac
Antoine Hanna <hannaantoinebusiness@gmail.com>
2026-07-29 16:14:24 +0200

    [GP] Add health component

---
+++ Source/TurnBasedJam/Public/Logic/Health/HealthComponent.h
@@ -0,0 +1,59 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HealthComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class TURNBASEDJAM_API UHealthComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UHealthComponent();
+
+	UFUNCTION(BlueprintCallable)
+	void InitializeComponent() override;
+	
+	UFUNCTION(BlueprintCallable)
+	void TakeDamage(float Damage);
+	UFUNCTION(BlueprintCallable)
+	void Heal(float Heal);
+
+	UFUNCTION(BlueprintCallable)
+	void Die();
+	
+	UFUNCTION()
+	float GetMaxHealth() const;
+	UFUNCTION()
+	float GetHealth() const;
+	
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FDamagedSignature);
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FHealedSignature);
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FHealthChangedSignature, float, Health, float, MaxHealth);
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FDiedSignature);
+	
+	UPROPERTY(BlueprintAssignable)
+	FDamagedSignature Damaged;
+	
+	UPROPERTY(BlueprintAssignable)
+	FHealedSignature Healed;
+	
+	UPROPERTY(BlueprintAssignable)
+	FHealthChangedSignature HealthChanged;
+	
+	UPROPERTY(BlueprintAssignable)
+	FDiedSignature Died;
+	
+private:
+	
+	UPROPERTY(EditAnywhere)
+	float MaxHealth;
+	
+	UPROPERTY()
+	float Health;
+};
